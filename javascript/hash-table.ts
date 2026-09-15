cddbb4d8c6b219a61af76626b9d12197d91e90d4
Amiola Demilade <amiolademilade@gmail.com>
2026-09-15 19:08:26 +0100

    done-with-the-rehashing-logic

--- javascript/hash-table.ts
+++ javascript/hash-table.ts
@@ -4,7 +4,19 @@ interface hashTableInterface {
     values: string[]
 }
 
-const myHashTableAsArray:hashTableInterface[] = []
+let myHashTableAsArray:hashTableInterface[] = []
+
+
+//set bucket size it can be 5, 10, 15, 20, 100,1000, 10000,etc. Your size will determine the point where the the hash needs to be rehashed 
+let bucketSize = 10
+
+
+//temporary storage 
+let tempStorage:string[] = []
+
+//counter variable to track how many items has been added to the hash table
+let counter:number = 0
+
 
 //the hashcode is the key which is generated everytime you pass a value it to the hashfunction
 function hashCodeFunction(value: string) {
@@ -14,12 +26,79 @@ function hashCodeFunction(value: string) {
          key += value.charCodeAt(i)       
         }
 
-        return key % 10;
+        return key % bucketSize;
 }
+
+//load factor is the mathematics that i used to get the point where the hash needs to be rehashed or resized it is set to 0.75 or 75%
+function loadFactor(totalSizeOfBucketCapacity:number, sizeOfValueinBucket: number) {
+    return sizeOfValueinBucket / totalSizeOfBucketCapacity;
+}
+
+
+function rehash() {
+    const existingValues:string[] = getBucket(); 
+    // add more buckets to the current bucket 
+    bucketSize *= 2; // Double the bucket size
+
+    // 3. Reset table storage and tracking counters
+    myHashTableAsArray = [];
+
+
+    // then re add the values so it can get new position cause the size of the hash table is now different 
+    for (let i = 0; i < existingValues.length; i++) {
+        addRehashValue(existingValues[i]);
+    }
+
+    console.log("Rehashing done. New bucket size: " + bucketSize, myHashTableAsArray);
+}
+
+
+
+function getBucket() {
+// temporary hash table array to track how many itmes has been added 
+    let myHashTableResultArray:string[] = []
+
+     //This for loop will loop over the array to check if there is something in each postion. If there is push them into myHashTableResultArray a a temporary array to calculate the load factor
+    for (let i = 0; i < myHashTableAsArray.length; i++) {
+        if(!myHashTableAsArray[i]) {
+            continue
+        }
+
+        //puts each string into the array so you can get how many data has enterted into the hash table 
+        myHashTableResultArray.push(...myHashTableAsArray[i].values)
+
+    }
+
+    return myHashTableResultArray
+}
+
 
 //This is the function to add value to the hashmap DS and also handle the collission case scenariao too 
 function addValue(value: string) {
 
+    let key =  hashCodeFunction(value)
+    counter++
+
+
+    if (!myHashTableAsArray[key]) {
+        myHashTableAsArray[key] = {
+            values: []
+        };
+    }
+        
+    myHashTableAsArray[key].values.push(value);
+
+     if(counter /bucketSize > 0.75) {
+        rehash();
+    }
+
+   return {
+     isSaved: true,
+     key: key
+   }
+}
+
+function addRehashValue(value: string) {
     let key =  hashCodeFunction(value)
 
     if (!myHashTableAsArray[key]) {
@@ -27,13 +106,13 @@ function addValue(value: string) {
             values: []
         };
     }
-
+        
     myHashTableAsArray[key].values.push(value);
 
    return {
      isSaved: true,
      key: key
-   }
+   }    
 }
 
 //For checking/looking up a value in the hashmap on average case is constant time o(1) and for worstcase scenario is o(n).
@@ -68,26 +147,24 @@ function deleteValue(value: string) {
     return {
         key: key,
         value :  newArr
-
     }
 
 }
 
+
+console.log(addValue("amiola"))
 console.log(addValue("demilade"));
 console.log(addValue("mary"));
-console.log(addValue("john"))
+console.log(addValue("demilade"));
+console.log(addValue("amiola"))
+console.log(addValue("demilade"));
+console.log(addValue("mary"));
+console.log(addValue("demilade"));
+
+    let dataStored = myHashTableAsArray;
 
 
-console.log(lookUpValue("demilade"))
-console.log(deleteValue("demilade"))
 
-
-console.log(lookUpValue("demilade"))
-
-
-
-console.log( myHashTableAsArray)
-
-
+    console.log(bucketSize)
 
 // Amiola_dev
